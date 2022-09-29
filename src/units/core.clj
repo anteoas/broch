4f99da05ae2cb000923f9127e32c2398298bd269
Tormod Mathiesen <tormathi@gmail.com>
2022-09-29 11:05:43 +0200

    rename constructor fns

--- src/units/core.clj
+++ src/units/core.clj
@@ -78,9 +78,9 @@
 
 ;; Defining units
 
-(defn ->unit
+(defn unit
   ([measure symb scale-of-base]
-   (->unit measure symb scale-of-base 0))
+   (unit measure symb scale-of-base 0))
   ([measure symb scale-of-base trans-of-base]
    (let [unit (->Unit measure symb scale-of-base trans-of-base nil)]
      (impl/register-unit! unit)
@@ -88,8 +88,8 @@
        ([] unit)
        ([x] (impl/new-unit unit x))))))
 
-(defn ->derived
-  ([measure units] (->derived measure units nil))
+(defn derived
+  ([measure units] (derived measure units nil))
   ([measure units symb]
    (let [units (impl/ensure-basic units)
          derived (->Derived measure units symb nil)]
@@ -102,66 +102,66 @@
 
 
 ;; Units
-(def kilo (->unit :amount :k 1000))
+(def kilo (unit :amount :k 1000))
 
 ;; Length
-(def millimeters (->unit :length :mm 1/1000))
-(def centimeters (->unit :length :cm 1/100))
-(def meters (->unit :length :m 1))
-(def kilometers (->unit :length :km 1000))
-(def nautical-miles (->unit :length :NM 1852))
+(def millimeters (unit :length :mm 1/1000))
+(def centimeters (unit :length :cm 1/100))
+(def meters (unit :length :m 1))
+(def kilometers (unit :length :km 1000))
+(def nautical-miles (unit :length :NM 1852))
 
 ;; Stupid Length
-(def miles (->unit :length :mi 1609.344))
-(def yards (->unit :length :yd 0.9144))
-(def feet (->unit :length :ft 0.3048))
-(def inches (->unit :length :in 0.0254))
+(def miles (unit :length :mi 1609.344))
+(def yards (unit :length :yd 0.9144))
+(def feet (unit :length :ft 0.3048))
+(def inches (unit :length :in 0.0254))
 
 ;; Time
-(def seconds (->unit :time :s 1))
-(def minutes (->unit :time :min 60))
-(def hours (->unit :time :h 3600))
+(def seconds (unit :time :s 1))
+(def minutes (unit :time :min 60))
+(def hours (unit :time :h 3600))
 
 ;; Mass
-(def grams (->unit :mass :g 1/1000))
-(def kilograms (->unit :mass :kg 1))
-(def tonnes (->unit :mass :t 1000))
+(def grams (unit :mass :g 1/1000))
+(def kilograms (unit :mass :kg 1))
+(def tonnes (unit :mass :t 1000))
 
 ;; Temperature
-(def kelvin (->unit :thermodynamic-temperature :K 1))
-(def celsius (->unit :thermodynamic-temperature :C 1 273.15))
-(def fahrenheit (->unit :thermodynamic-temperature :F 5/9 459.67))
+(def kelvin (unit :thermodynamic-temperature :K 1))
+(def celsius (unit :thermodynamic-temperature :C 1 273.15))
+(def fahrenheit (unit :thermodynamic-temperature :F 5/9 459.67))
 
 ;; Other SI
-(def amperes (->unit :electric-current :A 1))
-(def moles (->unit :amount-of-substance :mol 1))
-(def candelas (->unit :luminous-intensity :cd 1))
+(def amperes (unit :electric-current :A 1))
+(def moles (unit :amount-of-substance :mol 1))
+(def candelas (unit :luminous-intensity :cd 1))
 
 ;; Area
-(def squared-millimeters (->derived :area {millimeters 2}))
-(def squared-centimeters (->derived :area {centimeters 2}))
-(def squared-meters (->derived :area {meters 2}))
-(def squared-kilometers (->derived :area {kilometers 2}))
-(def squared-miles (->derived :area {miles 2}))
-(def squared-yards (->derived :area {yards 2}))
-(def squared-feet (->derived :area {feet 2}))
-(def squared-inches (->derived :area {inches 2}))
+(def squared-millimeters (derived :area {millimeters 2}))
+(def squared-centimeters (derived :area {centimeters 2}))
+(def squared-meters (derived :area {meters 2}))
+(def squared-kilometers (derived :area {kilometers 2}))
+(def squared-miles (derived :area {miles 2}))
+(def squared-yards (derived :area {yards 2}))
+(def squared-feet (derived :area {feet 2}))
+(def squared-inches (derived :area {inches 2}))
 
 ;; Speed
-(def kilometers-per-hour (->derived :speed {kilometers 1 hours -1}))
-(def meters-per-second (->derived :speed {meters 1 seconds -1}))
-(def miles-per-hour (->derived :speed {miles 1 hours -1}))
+(def kilometers-per-hour (derived :speed {kilometers 1 hours -1}))
+(def meters-per-second (derived :speed {meters 1 seconds -1}))
+(def miles-per-hour (derived :speed {miles 1 hours -1}))
 
 ;; Acceleration
-(def meters-per-second2 (->derived :acceleration {meters 1 seconds -2}))
+(def meters-per-second2 (derived :acceleration {meters 1 seconds -2}))
 
 ;; Force
-(def newtons (->derived :force {kilograms 1 meters 1 seconds -2} :N))
+(def newtons (derived :force {kilograms 1 meters 1 seconds -2} :N))
 
 ;; Power & Energy
-(def joules (->derived :energy {newtons 1 meters 1} :J))
-(def watts (->derived :power {joules 1 seconds -1} :W))
-(def kilowatts (->derived :power {kilo 1 watts 1} :kW))
-(def watt-hours (->derived :energy {watts 1 hours 1} :Wh))
-(def kilowatt-hours (->derived :energy {kilowatts 1 hours 1} :kWh))
+(def joules (derived :energy {newtons 1 meters 1} :J))
+(def watts (derived :power {joules 1 seconds -1} :W))
+(def kilowatts (derived :power {kilo 1 watts 1} :kW))
+(def watt-hours (derived :energy {watts 1 hours 1} :Wh))
+(def kilowatt-hours (derived :energy {kilowatts 1 hours 1} :kWh))
 
